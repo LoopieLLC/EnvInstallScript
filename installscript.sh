3fbd66053f38a5c017bc0037af0fb0e3db2e9e6b
loopiellcdev <42252089+loopiellcdev@users.noreply.github.com>
2018-08-14 19:28:46 -0400

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -58,7 +58,7 @@ echo
 echo "***install ruby on rails***"
 echo
 sudo apt-get update
-sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install curl -y
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 cd /tmp
